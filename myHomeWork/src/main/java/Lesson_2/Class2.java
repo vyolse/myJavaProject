a9ed5fa45b9c9bdfd66cb37996bc6825d82b032e
vyolse <vyolse@gmail.com>
2022-03-11 12:58:32 +0300

    Commit

---
+++ myHomeWork/src/main/java/Lesson_2/Class2.java
@@ -0,0 +1,14 @@
+package Lesson_2;
+
+public class Class2 {
+    public static void main(String[] args) {
+        chislo(-20);
+    }
+    public static void chislo(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+}
